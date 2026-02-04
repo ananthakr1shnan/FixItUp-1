85033cdf8f311a24671bfe97fcab3dac975164be
S Varun Balaji <varun@nalashaa.net>
2026-02-04 11:45:25 +0530

    Update Program.cs for new Db Setup

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@ using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var mysqlUrl = Environment.GetEnvironmentVariable("MYSQL_URL")
-    ?? throw new Exception("MYSQL_URL environment variable not found");
+var mysqlUrl = Environment.GetEnvironmentVariable("MYSQL_URL");
+if (string.IsNullOrEmpty(mysqlUrl))
+{
+    throw new Exception("MYSQL_URL environment variable not found");
+}
 
-var csb = new MySqlConnectionStringBuilder(mysqlUrl);
+var uri = new Uri(mysqlUrl);
+
+var userInfo = uri.UserInfo.Split(':', 2);
+var username = userInfo[0];
+var password = userInfo.Length > 1 ? userInfo[1] : "";
+
+var connectionString =
+    $"Server={uri.Host};" +
+    $"Port={uri.Port};" +
+    $"Database={uri.AbsolutePath.TrimStart('/')};" +
+    $"User={username};" +
+    $"Password={password};" +
+    $"SslMode=Preferred;";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
-        csb.ConnectionString,
-        ServerVersion.AutoDetect(csb.ConnectionString)
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
     )
 );
 
